cf7b9b522cada4750ad3e3ca574ca003192cf1ac
Aurelien Brabant <aurelien@brabant.dev>
2025-03-01 11:33:58 +0100

    fix: get rid of jsoncpp bs

--- daemon/src/main.cpp
+++ daemon/src/main.cpp
@@ -6,7 +6,6 @@
 #include <QtSql/qsqldatabase.h>
 #include <arpa/inet.h>
 #include <cmark.h>
-#include <jsoncpp/json/reader.h>
 #include <qfontdatabase.h>
 #include <qlist.h>
 #include <qlocalserver.h>
