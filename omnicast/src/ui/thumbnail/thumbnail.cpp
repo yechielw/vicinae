db602ebb6d847b8ebcc6092298770071bdecfef6
Aurelien Brabant <aurelien@brabant.dev>
2025-07-25 14:57:04 +0200

    feat: split up thumbnail code

---
+++ omnicast/src/ui/thumbnail/thumbnail.cpp
@@ -0,0 +1,100 @@
+#include "thumbnail.hpp"
+
+void Thumbnail::resizeEvent(QResizeEvent *event) {
+  QWidget::resizeEvent(event);
+  m_content->setFixedSize(size());
+  m_content->move(0, 0);
+  m_content->show();
+  m_placeholder->stackUnder(m_content);
+}
+
+void Thumbnail::paintEvent(QPaintEvent *event) {
+  OmniPainter painter(this);
+
+  painter.setRenderHint(QPainter::Antialiasing);
+
+  painter.setThemePen(SemanticColor::Border);
+  painter.setThemeBrush(SemanticColor::MainHoverBackground);
+  painter.drawRoundedRect(rect(), m_borderRadius, m_borderRadius);
+
+  m_opacityEffect->setOpacity(underMouse() && m_clickable ? 0.8 : 1);
+
+  QWidget::paintEvent(event);
+}
+
+void Thumbnail::setupUI() {
+  auto layout = new QVBoxLayout;
+
+  m_placeholder->setFixedSize(25, 25);
+  m_placeholder->setUrl(BuiltinOmniIconUrl("image").setFill(SemanticColor::TextSecondary));
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->addWidget(m_placeholder, 0, Qt::AlignCenter);
+
+  setLayout(layout);
+}
+
+void Thumbnail::mousePressEvent(QMouseEvent *event) {
+  if (event->button() == Qt::MouseButton::LeftButton && m_clickable) {
+    emit clicked();
+    return;
+  }
+
+  QWidget::mousePressEvent(event);
+}
+
+void Thumbnail::setClickable(bool clickable) {
+  m_clickable = clickable;
+  update();
+}
+
+void Thumbnail::setImage(const OmniIconUrl &url) {
+  m_content->setUrl(OmniIconUrl(url).setMask(OmniPainter::RoundedRectangleMask));
+}
+
+void Thumbnail::setRadius(int radius) {
+  m_borderRadius = radius;
+  update();
+}
+
+Thumbnail::Thumbnail(QWidget *parent) : QWidget(parent) {
+  setAttribute(Qt::WA_Hover);
+  setupUI();
+  setGraphicsEffect(m_opacityEffect);
+  m_opacityEffect->setOpacity(1);
+}
+
+void ImagePreviewDialogWidget::resizeEvent(QResizeEvent *event) {
+  qDebug() << "resize preview dialog" << event;
+  QWidget::resizeEvent(event);
+}
+
+QSize ImagePreviewDialogWidget::sizeHint() const {
+  if (auto widget = parentWidget()) {
+    auto parentSize = widget->size();
+    int targetHeight = parentSize.height() - m_padding * 2;
+    int width = targetHeight * m_aspectRatio;
+
+    qDebug() << "parent size" << parentSize;
+
+    return {width, targetHeight};
+  }
+
+  return {100, 100};
+}
+
+QSize ImagePreviewDialogWidget::minimumSizeHint() const { return sizeHint(); }
+
+void ImagePreviewDialogWidget::setupUI() {
+  auto layout = new QVBoxLayout;
+
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->addWidget(m_thumbnail);
+  setLayout(layout);
+}
+
+void ImagePreviewDialogWidget::setAspectRatio(double ratio) { m_aspectRatio = ratio; }
+
+ImagePreviewDialogWidget::ImagePreviewDialogWidget(const OmniIconUrl &icon) {
+  setupUI();
+  m_thumbnail->setImage(icon);
+}
--- omnicast/src/ui/thumbnail/thumbnail.hpp
+++ omnicast/src/ui/thumbnail/thumbnail.hpp
@@ -1,10 +1,7 @@
 #pragma once
-#include "navigation-controller.hpp"
 #include "omni-icon.hpp"
-#include "theme.hpp"
 #include "ui/dialog.hpp"
 #include "ui/image/omnimg.hpp"
-#include "ui/omni-painter.hpp"
 #include <qboxlayout.h>
 #include <qevent.h>
 #include <qgraphicseffect.h>
@@ -26,67 +23,17 @@ class Thumbnail : public QWidget {
   bool m_clickable = false;
   QGraphicsOpacityEffect *m_opacityEffect = new QGraphicsOpacityEffect(this);
 
-  void resizeEvent(QResizeEvent *event) override {
-    QWidget::resizeEvent(event);
-    m_content->setFixedSize(size());
-    m_content->move(0, 0);
-    m_content->show();
-    m_placeholder->stackUnder(m_content);
-  }
-
-  void paintEvent(QPaintEvent *event) override {
-    OmniPainter painter(this);
-
-    painter.setRenderHint(QPainter::Antialiasing);
-
-    painter.setThemePen(SemanticColor::Border);
-    painter.setThemeBrush(SemanticColor::MainHoverBackground);
-    painter.drawRoundedRect(rect(), m_borderRadius, m_borderRadius);
-
-    m_opacityEffect->setOpacity(underMouse() && m_clickable ? 0.8 : 1);
-
-    QWidget::paintEvent(event);
-  }
-
-  void setupUI() {
-    auto layout = new QVBoxLayout;
-
-    m_placeholder->setFixedSize(25, 25);
-    m_placeholder->setUrl(BuiltinOmniIconUrl("image").setFill(SemanticColor::TextSecondary));
-    layout->setContentsMargins(0, 0, 0, 0);
-    layout->addWidget(m_placeholder, 0, Qt::AlignCenter);
-
-    setLayout(layout);
-  }
-
-  void mousePressEvent(QMouseEvent *event) override {
-    if (event->button() == Qt::MouseButton::LeftButton && m_clickable) {
-      emit clicked();
-      return;
-    }
-
-    QWidget::mousePressEvent(event);
-  }
+  void resizeEvent(QResizeEvent *event) override;
+  void paintEvent(QPaintEvent *event) override;
+  void setupUI();
+  void mousePressEvent(QMouseEvent *event) override;
 
 public:
-  void setClickable(bool clickable) {
-    m_clickable = clickable;
-    update();
-  }
-  void setImage(const OmniIconUrl &url) {
-    m_content->setUrl(OmniIconUrl(url).setMask(OmniPainter::RoundedRectangleMask));
-  }
-  void setRadius(int radius) {
-    m_borderRadius = radius;
-    update();
-  }
+  void setClickable(bool clickable);
+  void setImage(const OmniIconUrl &url);
+  void setRadius(int radius);
 
-  Thumbnail(QWidget *parent = nullptr) : QWidget(parent) {
-    setAttribute(Qt::WA_Hover);
-    setupUI();
-    setGraphicsEffect(m_opacityEffect);
-    m_opacityEffect->setOpacity(1);
-  }
+  Thumbnail(QWidget *parent = nullptr);
 
 signals:
   void clicked() const;
@@ -97,40 +44,13 @@ class ImagePreviewDialogWidget : public DialogContentWidget {
   int m_padding = 20;
   double m_aspectRatio = 16 / 9.f;
 
-  void resizeEvent(QResizeEvent *event) override {
-    qDebug() << "resize preview dialog" << event;
-    QWidget::resizeEvent(event);
-  }
-
-  QSize sizeHint() const override {
-    if (auto widget = parentWidget()) {
-      auto parentSize = widget->size();
-      int targetHeight = parentSize.height() - m_padding * 2;
-      int width = targetHeight * m_aspectRatio;
-
-      qDebug() << "parent size" << parentSize;
-
-      return {width, targetHeight};
-    }
-
-    return {100, 100};
-  }
-
-  QSize minimumSizeHint() const override { return sizeHint(); }
-
-  void setupUI() {
-    auto layout = new QVBoxLayout;
-
-    layout->setContentsMargins(0, 0, 0, 0);
-    layout->addWidget(m_thumbnail);
-    setLayout(layout);
-  }
+  void resizeEvent(QResizeEvent *event) override;
+  QSize sizeHint() const override;
+  QSize minimumSizeHint() const override;
+  void setupUI();
 
 public:
-  void setAspectRatio(double ratio) { m_aspectRatio = ratio; }
+  void setAspectRatio(double ratio);
 
-  ImagePreviewDialogWidget(const OmniIconUrl &icon) {
-    setupUI();
-    m_thumbnail->setImage(icon);
-  }
+  ImagePreviewDialogWidget(const OmniIconUrl &icon);
 };
