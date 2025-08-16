6cf22a7292e910d1023bfcb3f9b26d21dcfe306a
Elliot Nash <elliotnash@gmail.com>
2025-08-15 17:26:46 -0700

    Manually resolve icons if not found by QIcon::fromTheme

--- vicinae/src/ui/image/qicon-image-loader.hpp
+++ vicinae/src/ui/image/qicon-image-loader.hpp
@@ -1,9 +1,13 @@
 #pragma once
 #include "ui/image/image.hpp"
+#include <QFile>
 
 class QIconImageLoader : public AbstractImageLoader {
   QString m_icon;
   std::optional<QString> m_theme;
+
+private:
+  QIcon loadIconFromFileSystem(const QString &iconName);
 
 public:
   void render(const RenderConfig &config) override;
