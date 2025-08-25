ebb5523604aac3fdac866d4c483b8c9ae1ff2844
Aurelien Brabant <aurelien@brabant.dev>
2025-08-26 00:52:10 +0200

    feat(boilerplate): do not use json to generate package.json boilerplate

--- vicinae/src/services/extension-boilerplate-generator/extension-boilerplate-generator.cpp
+++ vicinae/src/services/extension-boilerplate-generator/extension-boilerplate-generator.cpp
@@ -1,13 +1,22 @@
 #include "services/extension-boilerplate-generator/extension-boilerplate-generator.hpp"
 #include "common.hpp"
 #include "utils/utils.hpp"
-#include <absl/strings/internal/str_format/extension.h>
 #include <filesystem>
 #include <qfile.h>
-#include <qjsondocument.h>
-#include <qjsonparseerror.h>
-#include <qjsonobject.h>
-#include <qjsonarray.h>
+
+namespace fs = std::filesystem;
+
+#define PLACEHOLDER(x) "%" x "%"
+
+static const QString COMMAND_JSON_TEMPLATE = R"(    {
+      "name": "%NAME%",
+      "title": "%TITLE%",
+      "subtitle": "%SUBTITLE%",
+      "description": "%DESCRIPTION%",
+      "mode": "%MODE%"
+    })";
+
+static const QString COMMAND_LIST_JSON_TEMPLATE = "[\n%1\n  ]";
 
 static const std::vector<CommandBoilerplate> CMD_TEMPLATE_LIST = {
     CommandBoilerplate{.resource = ":boilerplate/tmpl-list", .name = "Simple List", .mode = CommandModeView},
@@ -19,8 +28,6 @@ static const std::vector<CommandBoilerplate> CMD_TEMPLATE_LIST = {
         .resource = ":boilerplate/tmpl-simple-detail", .name = "Simple Detail", .mode = CommandModeView},
     CommandBoilerplate{.resource = ":boilerplate/tmpl-no-view", .name = "No View", .mode = CommandModeNoView},
 };
-
-namespace fs = std::filesystem;
 
 const std::vector<CommandBoilerplate> &ExtensionBoilerplateGenerator::commandBoilerplates() const {
   return CMD_TEMPLATE_LIST;
@@ -59,30 +66,12 @@ ExtensionBoilerplateGenerator::generate(const fs::path &targetDir, const Extensi
     return std::unexpected("Cannot open package.json boilerplate");
   }
 
-  QJsonParseError parseError;
-  auto json = QJsonDocument::fromJson(packageJson.readAll(), &parseError);
-
-  if (parseError.error != QJsonParseError::NoError) {
-    return std::unexpected(QString("Failed to parse package.json: %1").arg(parseError.errorString()));
-  }
-
-  auto manifest = json.object();
-  auto deps = manifest.value("dependencies").toObject();
-  QJsonArray cmds;
-  QString version(VICINAE_GIT_TAG);
-
-  if (!version.isEmpty()) version[0] = '^';
-
-  deps["@vicinae/api"] = version;
+  QString manifest = packageJson.readAll();
+  QStringList cmdStrings;
 
   for (const auto &cmd : config.commands) {
-    QJsonObject obj;
-    QString name = slugify(cmd.title);
 
-    obj["title"] = cmd.title;
-    obj["name"] = name;
-    obj["subtitle"] = cmd.subtitle;
-    obj["description"] = cmd.description.simplified();
+    QString name = slugify(cmd.title);
 
     auto pred = [&](auto &&tmpl) { return tmpl.resource == cmd.templateId; };
     auto it = std::ranges::find_if(CMD_TEMPLATE_LIST, pred);
@@ -91,34 +80,39 @@ ExtensionBoilerplateGenerator::generate(const fs::path &targetDir, const Extensi
       return std::unexpected(QString("Unknown template with id %1").arg(cmd.templateId));
     }
 
-    // QString ext = it->mode == CommandModeView ? "tsx" : "ts";
-    QString ext = "tsx";
+    QString mode = it->mode == CommandModeView ? "view" : "no-view";
+    QString cmdString = QString(COMMAND_JSON_TEMPLATE)
+                            .replace(PLACEHOLDER("NAME"), name.simplified())
+                            .replace(PLACEHOLDER("TITLE"), cmd.title.simplified())
+                            .replace(PLACEHOLDER("SUBTITLE"), cmd.subtitle.simplified())
+                            .replace(PLACEHOLDER("DESCRIPTION"), cmd.description.simplified())
+                            .replace(PLACEHOLDER("MODE"), mode);
+
+    QString ext = it->mode == CommandModeView ? "tsx" : "ts";
     QString filename = QString("%1.%2").arg(name).arg(ext);
 
-    obj["mode"] = it->mode == CommandModeView ? "view" : "no-view";
     userCopy(it->resource, QString::fromStdString(srcDir / filename.toStdString()));
-
-    cmds.push_back(obj);
+    cmdStrings << cmdString;
   }
 
-  manifest["author"] = config.author;
-  manifest["title"] = config.title;
-  manifest["description"] = config.description;
-  manifest["name"] = extName;
-  manifest["commands"] = cmds;
-  manifest["dependencies"] = deps;
+  QString version(VICINAE_GIT_TAG);
+
+  if (!version.isEmpty()) version[0] = '^';
+
+  // we don't use QJson because key order would not be preserved
+  manifest.replace(PLACEHOLDER("NAME"), extName)
+      .replace(PLACEHOLDER("TITLE"), config.title.simplified())
+      .replace(PLACEHOLDER("DESCRIPTION"), config.description.simplified())
+      .replace(PLACEHOLDER("AUTHOR"), config.author.simplified())
+      .replace(PLACEHOLDER("VICINAE_VERSION"), version)
+      .replace(PLACEHOLDER("COMMAND_LIST"), COMMAND_LIST_JSON_TEMPLATE.arg(cmdStrings.join(",\n    ")));
 
   {
-    QJsonDocument finalManifest;
     QFile file(extDir / "package.json");
 
-    finalManifest.setObject(manifest);
+    if (!file.open(QIODevice::WriteOnly)) { return std::unexpected(QString("Failed to write manifest")); }
 
-    if (!file.open(QIODevice::WriteOnly)) {
-      return std::unexpected(QString("Failed to parse package.json: %1").arg(parseError.errorString()));
-    }
-
-    file.write(finalManifest.toJson());
+    file.write(manifest.toUtf8());
   }
 
   userCopy(":boilerplate/tsconfig.json", QString::fromStdString(extDir / "tsconfig.json"));
